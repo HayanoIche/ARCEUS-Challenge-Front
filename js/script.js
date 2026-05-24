905dc79d2433ec862c8fc100bdf9d448832afec7
HayanoIche <ayanoiichi@gmail.com>
2026-05-24 17:46:44 -0300

    feat: mensagem enviando

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const menu = document.getElementById("menu");
+const nav = document.querySelector(".nav-bar");
+
+menu.addEventListener("click", () =>
+{
+    menu.classList.toggle("open");
+    nav.classList.toggle("open");
+});
+
+document.querySelectorAll('.faq__container details').forEach(detail => {
+    detail.addEventListener('toggle', () =>
+    {
+        if (detail.open)
+        {
+            document.querySelectorAll('.faq__container details').forEach(other =>
+            {
+                if (other !== detail)
+                {
+                    other.removeAttribute('open')
+                };
+            });
+        }
+    });
+});
+
+const formulario = document.querySelector("form"); 
+
+if (formulario)
+{
+    formulario.addEventListener("submit", (e) =>
+    {
+        e.preventDefault();
+        
+        alert("Mensagem enviada com sucesso!");
+        
+        formulario.reset();
+    });
+}
